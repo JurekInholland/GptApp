9f4aac04b55f57df860b91510df6c327ae104f79
Jurek Baumann <jurekbaumann@me.com>
2023-03-25 02:45:38 +0100

    web endpoint

---
+++ Api/Controllers/WebAppController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+public class WebAppController
+{
+    private static readonly string IndexHtml = System.IO.File.ReadAllText("wwwroot/index.html");
+
+    [HttpGet("/{route?}", Name = nameof(Index))]
+    public IActionResult Index([FromRoute] string? route = "")
+    {
+        return new ContentResult
+        {
+            Content = IndexHtml,
+
+            ContentType = "text/html",
+            StatusCode = 200
+        };
+    }
+}
